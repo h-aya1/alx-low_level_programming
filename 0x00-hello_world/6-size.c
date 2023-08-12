aa54e245bf51ff5a760c39775fc26895a297d3ca
h-aya1 <hayata.rezak@gmail.com>
2023-08-12 01:30:47 -0700

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-	printf("Size of char: %u byte(s)", sizeof(char));
-	printf("Size of int: %u byte(s)", sizeof(int));
-	printf("Size of long int: %u byte(s)", sizeof(long int));
-	printf("Size of long long int: %u byte(s)", sizeof(long long int));
-	printf("Size os float: %u byte(s)", sizeof(float));
-	return (0);
+        printf("Size of char: %u byte(s)", sizeof(char));
+        printf("Size of int: %u byte(s)", sizeof(int));
+        printf("Size of long int: %u byte(s)", sizeof(long int));
+        printf("Size of long long int: %u byte(s)", sizeof(long long int));
+        printf("Size os float: %u byte(s)", sizeof(float));
+        return (0);
 
 }
