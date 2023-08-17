609c31006818bce9a70d36a23043f856d66524e5
h-aya1 <hayata.rezak@gmail.com>
2023-08-17 08:05:31 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - absolute value function
+ * @int: parameter
+ * Return: 0
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+	{
+	return (-i);
+	}
+	else
+	{
+		return (i);
+	}
+}
