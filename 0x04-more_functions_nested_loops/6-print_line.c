7c6e8421da7a718cda76c9730c672275f054c20f
h-aya1 <hayata.rezak@gmail.com>
2023-08-18 13:27:11 -0700

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - print _ n times
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 0 ; i <= n ; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
