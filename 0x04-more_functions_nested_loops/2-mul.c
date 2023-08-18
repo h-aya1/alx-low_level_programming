f62f1328250afb68b2e379d222f21887ff3735f0
h-aya1 <hayata.rezak@gmail.com>
2023-08-18 10:39:39 -0700

    multiplies

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * mul - multplies the numbers
+ * @a: parameter
+ * @b: another parameter
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int c = a * b;
+	putchar(c);
+	return (c);
+}
+
