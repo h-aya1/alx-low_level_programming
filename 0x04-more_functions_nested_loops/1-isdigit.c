8581e5fbb3a38a5cf9351a061a8bb28470625329
h-aya1 <hayata.rezak@gmail.com>
2023-08-18 10:17:58 -0700

    digit checker

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks gor 0 to 9 digitd
+ * @c: parameter
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
