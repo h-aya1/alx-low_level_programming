eb3bd641bd48aac37639aa848ebd9bbfd93f3832
h-aya1 <hayata.rezak@gmail.com>
2023-08-21 08:28:53 -0700

    diagonalss

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,11 +19,9 @@ void print_diagonal(int n)
 		{
 			for (j = 0 ; j <= i ; j++)
 			{
-				if (j == 1)
-					_putchar('\\');
-				else if (j < i)
-					_putchar(' ');
+				_putchar(' ');
 			}
+			_putchar('\\');
 			_putchar('\n');
 		}
 	}
